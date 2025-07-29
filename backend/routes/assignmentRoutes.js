cf02b848732742f635c786620456c57b64735e45
AtharvInnosquares <atharv.desai@innosquares.com>
2025-07-29 13:46:40 +0530

    Create Django folder

--- backend/routes/assignmentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const assignmentController = require('../controllers/assignmentController');
-
-router.post('/assign', assignmentController.assignTaskToVendors);
-
-module.exports = router;  // <-- This must be present!
